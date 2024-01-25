5000fe6a84f23f02faea6c6f6b08f862ac11528f
obaraelijah <elijahobara357@gmail.com>
2024-01-25 20:11:06 +0300

    feat: actix_web project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+use prod_craft::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8080")?; 
+    run(listener)?.await
 }
