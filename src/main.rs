f24dda38c139c0f3fe758fb62e423d5118004170
obaraelijah <elijahobara357@gmail.com>
2024-01-27 15:12:42 +0300

    feat: configured tracing

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
 use std::net::TcpListener;
 use prod_craft::startup::run;
 use prod_craft::configuration::get_configuration;
-use sqlx::PgPool;
-use env_logger::Env;
+use sqlx::postgres::PgPool;
+use prod_craft::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
-    
-    let configuration = get_configuration().expect("Failed toread configuration");
+    let subscriber = get_subscriber("prod_craft".into(), "info".into());
+    init_subscriber(subscriber);
+
+    let configuration = get_configuration().expect("Failed to read configuration");
     let connection = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?; 
+
     run(listener, connection)?.await
 }
