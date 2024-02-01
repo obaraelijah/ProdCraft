20ef4d9262078bbd8e45f906d85229e5193fa9b0
obaraelijah <elijahobara357@gmail.com>
2024-02-01 14:58:40 +0300

    feat: configuring email links

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -5,6 +5,7 @@ use prod_craft::telemetry::{get_subscriber, init_subscriber};
 use once_cell::sync::Lazy;
 use prod_craft::startup::get_connection_pool;
 use prod_craft::startup::Application;
+use wiremock::MockServer;
 
 // Ensure that the `tracing` stack is only initialised once using `once_cell`
 static TRACING: Lazy<()> = Lazy::new(|| {
@@ -22,6 +23,7 @@ static TRACING: Lazy<()> = Lazy::new(|| {
 pub struct TestApp {
     pub address: String,
     pub db_pool: PgPool,
+    pub email_server: MockServer,
 }
 
 impl TestApp {
@@ -39,11 +41,14 @@ impl TestApp {
 pub async fn spawn_app() -> TestApp {
     // The first time `initialize` is invoked the code in `TRACING` is executed.
     // All other invocations will instead skip execution.
+    let email_server = MockServer::start().await;
+
     Lazy::force(&TRACING);
 
     let configuration = {
         let mut c = get_configuration().expect("Failed to read configuration.");
         c.database.database_name = Uuid::new_v4().to_string();
+        c.email_client.base_url = email_server.uri();
         c.application.port = 0;
         c
     };
@@ -60,7 +65,8 @@ pub async fn spawn_app() -> TestApp {
 
     TestApp {
         address,
-        db_pool: get_connection_pool(&configuration.database)
+        db_pool: get_connection_pool(&configuration.database),
+        email_server,
     }
 }
 
