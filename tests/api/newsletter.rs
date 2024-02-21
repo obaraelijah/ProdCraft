bd83069dd71d9c90a6cdd776efa817a3a18ef7a3
obaraelijah <elijahobara357@gmail.com>
2024-02-21 13:26:34 +0300

    test: more intergrated tests for newletter

--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -59,12 +59,7 @@ async fn newsletters_are_not_delivered_to_unconfirmed_subscribers() {
             "html": "<p>Newsletter body as HTML</p>",
         }
     });
-    let response = reqwest::Client::new()
-        .post(&format!("{}/newsletters", &app.address))
-        .json(&newsletter_request_body)
-        .send()
-        .await
-        .expect("Failed to execute request.");
+    let response = app.post_newsletters(newsletter_request_body).await;
 
     assert_eq!(response.status().as_u16(), 200);
     // Mock verifies on Drop that we haven't sent the newsletter email
@@ -89,12 +84,7 @@ async fn newsletters_are_delivered_to_confirmed_subscribers() {
                 "html": "<p>Newsletter body as HTML</p>",
             }
         });
-        let response = reqwest::Client::new()
-            .post(&format!("{}/newsletters", &app.address))
-            .json(&newsletter_request_body)
-            .send()
-            .await
-            .expect("Failed to execute request.");
+        let response = app.post_newsletters(newsletter_request_body).await;
 
         assert_eq!(response.status().as_u16(), 200);
         // Mock verifies on Drop that we have sent the newsletter email
@@ -120,18 +110,14 @@ async fn newsletters_returns_400_for_invalid_data() {
     ];
 
     for (invalid_body, error_message) in test_cases {
-        let response = reqwest::Client::new()
-            .post(&format!("{}/newsletters", &app.address))
-            .json(&invalid_body)
-            .send()
-            .await
-            .expect("Failed to execute request.")
+        let response = app.post_newsletters(invalid_body).await;
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
     }
 
-    assert_eq!(
-        400,
-        response.status().as_u16(),
-        "The API did not fail with 400 Bad Request when the payload was {}.",
-        error_message
-    );
 }
