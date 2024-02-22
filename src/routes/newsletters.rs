aefb056872f782e37ce231ea6f5a73a821739cea
obaraelijah <elijahobara357@gmail.com>
2024-02-22 13:18:53 +0300

    feat: password verification

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -5,7 +5,7 @@ use anyhow::Context;
 use crate::routes::error_chain_fmt;
 use crate::email_client::EmailClient;
 use crate::domain::SubscriberEmail;
-use secrecy::Secret;
+use secrecy::{Secret, ExposeSecret};
 use actix_web::http::header::{HeaderMap, HeaderValue}; 
 
 #[derive(thiserror::Error)]
@@ -58,14 +58,49 @@ impl ResponseError for PublishError {
     }
 }
 
+async fn validate_credentials(
+    credentials: Credentials,
+    pool: &PgPool,
+) -> Result<uuid::Uuid, PublishError> {
+    let user_id: Option<_> = sqlx::query!(
+        r#"
+        SELECT user_id
+        FROM users
+        WHERE username = $1 AND password = $2
+        "#,
+        credentials.username,
+        credentials.password.expose_secret()
+    )
+    .fetch_optional(pool)
+    .await
+    .context("Failed to perform a query to validate auth credentials.")
+    .map_err(PublishError::UnexpectedError)?;
+
+    user_id
+        .map(|row| row.user_id)
+        .ok_or_else(|| anyhow::anyhow!("Invalid username or password."))
+        .map_err(PublishError::AuthError)
+}
+
+
+#[tracing::instrument(
+    name = "Publish a newsletter issue",
+    skip(body, pool, email_client, request),
+    fields(username=tracing::field::Empty, user_id=tracing::field::Empty)
+)]
 pub async fn publish_newsletter(
     body: web::Json<BodyData>,
     pool: web::Data<PgPool>,
     email_client: web::Data<EmailClient>,
     request: HttpRequest,
 ) -> Result<HttpResponse, PublishError> {
-    let _credentials = basic_authentication(request.headers())
+    let credentials = basic_authentication(request.headers())
         .map_err(PublishError::AuthError)?;
+    tracing::Span::current().record(
+        "username",
+        &tracing::field::display(&credentials.username)
+    );
+    let user_id = validate_credentials(credentials, &pool).await?;
     let subscribers = get_confirmed_subscribers(&pool).await?;
     for subscriber in subscribers {
         match subscriber {
@@ -151,3 +186,4 @@ fn basic_authentication(headers: &HeaderMap) -> Result<Credentials, anyhow::Erro
         })
 
 }
+
