e777158e5557d7cd94fafa8e24cecc74e9e24795
obaraelijah <elijahobara357@gmail.com>
2024-02-22 16:09:43 +0300

    feat: password hashing using argon2

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -7,7 +7,7 @@ use crate::email_client::EmailClient;
 use crate::domain::SubscriberEmail;
 use secrecy::{Secret, ExposeSecret};
 use actix_web::http::header::{HeaderMap, HeaderValue}; 
-use sha3::Digest;
+use argon2::{Algorithm, Argon2, Version, Params, PasswordHasher};
 
 #[derive(thiserror::Error)]
 pub enum PublishError {
@@ -63,28 +63,52 @@ async fn validate_credentials(
     credentials: Credentials,
     pool: &PgPool,
 ) -> Result<uuid::Uuid, PublishError> {
-    let password_hash = sha3::Sha3_256::digest(
-        credentials.password.expose_secret().as_bytes()
+    let hasher = argon2::Argon2::new(
+        Algorithm::Argon2id,
+        Version::V0x13,
+        Params::new(15000, 2, 1, None)
+            .context("Failed to build Argon2 parameters")
+            .map_err(PublishError::UnexpectedError)?,
     );
-    let password_hash = format!("{:x}", password_hash);
-    let user_id: Option<_> = sqlx::query!(
+    let row: Option<_> = sqlx::query!(
         r#"
-        SELECT user_id
+        SELECT user_id, password_hash, salt
         FROM users
-        WHERE username = $1 AND password = $2
+        WHERE username = $1 
         "#,
         credentials.username,
-        password_hash
     )
     .fetch_optional(pool)
     .await
-    .context("Failed to perform a query to validate auth credentials.")
+    .context("Failed to perform a query to retrieve stored credentials.")
     .map_err(PublishError::UnexpectedError)?;
 
-    user_id
-        .map(|row| row.user_id)
-        .ok_or_else(|| anyhow::anyhow!("Invalid username or password."))
-        .map_err(PublishError::AuthError)
+    let (expected_password_hash, user_id, salt) = match row {
+        Some(row) => (row.password_hash, row.user_id, row.salt),
+        None => {
+            return Err(PublishError::AuthError(anyhow::anyhow!(
+                "Unknown username."
+            )));
+        }
+    };
+
+    let password_hash = hasher
+        .hash_password(
+            credentials.password.expose_secret().as_bytes(),
+            &salt
+    )
+    .context("Failed to hash password")
+    .map_err(PublishError::UnexpectedError)?;
+
+    let password_hash = format!("{:x}", password_hash.hash.unwrap());
+    
+    if password_hash != expected_password_hash {
+        Err(PublishError::AuthError(anyhow::anyhow!(
+            "Invalid password."
+        )))
+    } else {
+        Ok(user_id)
+    }
 }
 
 
@@ -191,4 +215,3 @@ fn basic_authentication(headers: &HeaderMap) -> Result<Credentials, anyhow::Erro
         })
 
 }
-
