143a50c762270b7e83d2ad811d27e31ab06a6b4c
obaraelijah <elijahobara357@gmail.com>
2024-02-24 20:32:14 +0300

    feat: flash messages

--- src/startup.rs
+++ src/startup.rs
@@ -11,6 +11,10 @@ use sqlx::PgPool;
 use tracing_actix_web::TracingLogger;
 use sqlx::postgres::PgPoolOptions;
 use secrecy::Secret;
+use actix_web_flash_messages::FlashMessagesFramework;
+use actix_web_flash_messages::storage::CookieMessageStore;
+use secrecy::ExposeSecret;
+use actix_web::cookie::Key;
 
 pub struct Application {
     port: u16,
@@ -57,7 +61,7 @@ pub fn get_connection_pool(configuration: &DatabaseSettings) -> PgPool {
 pub struct ApplicationBaseUrl(pub String);
 
 
-pub fn run(
+fn run(
     listener: TcpListener, 
     db_pool: PgPool, 
     email_client: EmailClient, 
@@ -67,9 +71,12 @@ pub fn run(
     let db_pool = Data::new(db_pool);
     let email_client = Data::new(email_client);
     let base_url = Data::new(ApplicationBaseUrl(base_url));
+    let message_store = CookieMessageStore::builder(
+        Key::from(hmac_secret.expose_secret().as_bytes())
+    ).build();
+    let message_framework = FlashMessagesFramework::builder(message_store).build();
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions/confirm", web::get().to(confirm))
             .route("/subscriptions", web::post().to(subscribe))
@@ -81,6 +88,8 @@ pub fn run(
             .app_data(email_client.clone())
             .app_data(base_url.clone())
             .app_data(Data::new(hmac_secret.0.clone()))
+            .wrap(message_framework.clone())
+            .wrap(TracingLogger::default())
     })
     .listen(listener)?
     .run();
--- tests/api/login.rs
+++ tests/api/login.rs
@@ -11,12 +11,15 @@ async fn an_error_flash_message_is_set_on_failure() {
     });
     let response = app.post_login(&login_body).await;
 
-    let flash_cookie = response.cookies().find(|c| c.name() == "_flash").unwrap();
+    assert_is_redirect_to(&response, "/login");
 
     assert_eq!(response.status().as_u16(), 303);
-    assert_is_redirect_to(&response, "/login");
-    assert_eq!(flash_cookie.value(), "Authentication failed");
 
+    // Act - Part 2 - Follow the redirect
     let html_page = app.get_login_html().await;
-    assert!(!html_page.contains(r#"<p><i>Authentication failed</i></p>"#));
+    assert!(html_page.contains("<p><i>Authentication failed</i></p>"));
+
+    // Act - Part 3 - Reload the login page
+    let html_page = app.get_login_html().await;
+    assert!(!html_page.contains("<p><i>Authentication failed</i></p>"));
 }
