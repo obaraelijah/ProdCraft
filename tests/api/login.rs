53ab56836d81559b50e1fd81a23a6053d7c074e1
obaraelijah <elijahobara357@gmail.com>
2024-02-24 19:21:40 +0300

    test: cookies tests

--- tests/api/login.rs
+++ tests/api/login.rs
@@ -13,6 +13,9 @@ async fn an_error_flash_message_is_set_on_failure() {
     });
     let response = app.post_login(&login_body).await;
 
+    let flash_cookie = response.cookies().find(|c| c.name() == "_flash").unwrap();
+
     assert_eq!(response.status().as_u16(), 303);
     assert_is_redirect_to(&response, "/login");
+    assert_eq!(flash_cookie.value(), "Authentication failed");
 }
