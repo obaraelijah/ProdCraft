1f994b6ff6e8dd8f60e6d7245333697a24d7cf1a
obaraelijah <elijahobara357@gmail.com>
2024-02-26 21:47:42 +0300

    feat: idempotency

---
+++ src/idempotency/key.rs
@@ -0,0 +1,10 @@
+#[derive(Debug)]
+pub struct IdempotencyKey(String);
+
+impl TryFrom<String> for IdempotencyKey {
+    type Error = anyhow::Error;
+
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        todo!
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,4 @@ pub mod email_client;
 pub mod authentication;
 pub mod session_state;
 pub mod utils;
+pub mod idempotency;
