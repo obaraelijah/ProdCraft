3b480039039a9f137c78aed1c221502412434578
obaraelijah <elijahobara357@gmail.com>
2024-02-27 09:34:19 +0300

    featL impliment different rust conversiona for idempodenc key

--- src/idempotency/key.rs
+++ src/idempotency/key.rs
@@ -5,6 +5,27 @@ impl TryFrom<String> for IdempotencyKey {
     type Error = anyhow::Error;
 
     fn try_from(s: String) -> Result<Self, Self::Error> {
-        todo!
+        if s.is_empty() {
+            anyhow::bail!("The idempotency key cannot be empty");
+        }
+        let max_length = 50;
+        if s.len() >= max_length {
+            anyhow::bail!(
+                "The idempotency key must be shorter
+                than {max_length} characters");
+        }
+        Ok(Self(s))
+    }
+} 
+
+impl From<IdempotencyKey> for String {
+    fn from(k: IdempotencyKey) -> Self {
+        k.0
+    }
+}
+
+impl AsRef<str> for IdempotencyKey {
+    fn as_ref(&self) -> &str {
+        &self.0
     }
 }
